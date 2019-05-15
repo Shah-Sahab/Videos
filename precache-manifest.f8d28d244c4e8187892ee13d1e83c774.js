24c193e3d8a8bd41fb7793969a1d53b5e2405f55
Syed Ahmed Hussain <ahmedhussaindev@gmail.com>
2019-05-15 15:14:17 -0500

    Updates

---
+++ precache-manifest.f8d28d244c4e8187892ee13d1e83c774.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d04cae9d63d4c65d39dd9499e2bb62",
+    "url": "/Videos/index.html"
+  },
+  {
+    "revision": "a655da2a8d6294c27109",
+    "url": "/Videos/static/css/main.96c11df6.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/Videos/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "a655da2a8d6294c27109",
+    "url": "/Videos/static/js/main.7e081733.chunk.js"
+  },
+  {
+    "revision": "e082ff4dd3ff6df9afd0",
+    "url": "/Videos/static/js/runtime~main.274ca13e.js"
+  }
+]);
